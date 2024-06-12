2850b7cfdbab530ae10fd441bc8a2aeaf9c6265f
ValDark7 <valera1993931@inbox.ru>
2024-06-12 18:33:55 +0600

    first commit

--- server.js
+++ server.js
@@ -1,31 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const authRoutes = require("./routes/auth");
-const profileRoutes = require("./routes/profile");
 const cors = require("cors");
-const cartRoutes = require("./routes/cart");
+require("dotenv").config(); // Для использования переменных окружения
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000" }));
+// Middleware для парсинга JSON
+app.use(express.json());
 
-// Middleware
-app.use(bodyParser.json());
+app.use(cors());
 
-// Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/profile", profileRoutes);
-app.use("/api/carts", cartRoutes);
-
-// Connect to MongoDB
+// Подключение к MongoDB
+const dbURI = process.env.MONGODB_URI || "mongodb://localhost:27017/mydatabase";
 mongoose
-  .connect("mongodb://localhost:27017/mydatabase", {
+  .connect(dbURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
+// Убедитесь, что путь к маршрутам указан правильно
+
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/profile", require("./routes/profile"));
+app.use("/api/carts", require("./routes/cart"));
+
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
